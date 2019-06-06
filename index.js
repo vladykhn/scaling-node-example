560775d6c9fb2457125e8e7960c45834394f8845
Vladislav Yakukhin <vladislav.yakukhin@iqoption.com>
2019-06-06 11:27:07 +0300

    add nginx example

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const workerName = `Worker-${process.pid}`;
 const port = 3000;
 
 const requestHandler = (_, res) => {
+  console.log(`I (${workerName}) received request`);
   res.writeHead(200);
   res.end(`Hello! I am ${workerName}\n`);
 };
