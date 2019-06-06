159ac6caea2f6d6a1e24bf5b0086383091fdaf7f
Vladislav Yakukhin <vladislav.yakukhin@iqoption.com>
2019-06-06 10:55:46 +0300

    add pm2

---
+++ index.js
@@ -0,0 +1,13 @@
+const server = require("http");
+
+const workerName = `Worker-${process.pid}`;
+const port = 3000;
+
+const requestHandler = (_, res) => {
+  res.writeHead(200);
+  res.end(`Hello! I am ${workerName}\n`);
+};
+
+server
+  .createServer(requestHandler)
+  .listen(port, () => console.log(`${workerName} listening on port ${port}`));
